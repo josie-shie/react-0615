9e74d1bf241eb93ed991fc3b875721cdc5bcbb28
josie-shie <joe003218@gmail.com>
2021-06-22 23:27:16 +0800

    todo list

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,72 +1,134 @@
 import React, { useState } from 'react'
+import TodoAddForm from './TodoAddForm'
+import TodoList from './TodoList'
 
 function TodoApp() {
-  const [todo, setTodo] = useState('')
+  const [todoInput, setTodoInput] = useState('')
 
+  // 待辨事項每個的物件值
+  // todo = {id:number, text:string, completed:bool(default: false)}
   const [todos, setTodos] = useState([
-    { id: 998, text: '今天開始React', complete: false },
-    { id: 999, text: '買iphone', complete: false },
+    { 
+      id: 9999999, 
+      text: '努力學React', 
+      completed: true, 
+      edite: false 
+    },
+    {
+      id: 9999977,
+      text: '買iPhone或安卓手機',
+      completed: false,
+      edite: false,
+    },
   ])
 
-  const handelComplete = (id) => {
-    // 複合型狀態更新不管怎樣就是先考被陣列出來
+  // 另一個仿照資料庫的自動遞增的方式來產生id
+  // 每次要id值時呼叫increment()
+  // const [autoIncrement, setAutoIncrement] = useState(0)
+
+  // const increment = () => {
+  //   const newAutoIncrement = autoIncrement + 1
+  //   setAutoIncrement(newAutoIncrement)
+  //   return newAutoIncrement
+  // }
+
+  const handleEdited = (id) => {
     // 先由目前的todos拷貝出一個陣列
     const newTodos = [...todos]
 
     // 利用id值找到對應的todo項目的索引值
+    // 用findIndex的作法
     const index = newTodos.findIndex((item) => item.id === id)
 
+    // 如果有找到就切換completed的true/false
+    if (index !== -1) {
+      newTodos[index].edite = !newTodos[index].edite
+      // 設定回狀態值
+      setTodos(newTodos)
+    }
+  }
+
+  // 刪除使用
+  const handleDelete = (id) => {
+    // 先由目前的todos過濾掉"不要"有這個id的一個新陣列
+    const newTodos = todos.filter((item) => item.id !== id)
+    // 設定回狀態值
+    setTodos(newTodos)
+
+    // // for
+    // const newTodos = []
+
+    // for (let i = 0; i < todos.length; i++) {
+    //   if (todos[i].id === id) continue
+
+    //   newTodos.push(todos[i])
+    // }
+    // setTodos(newTodos)
+  }
+
+  // 完成使用
+  const handleCompleted = (id) => {
+    // 先由目前的todos拷貝出一個陣列
+    const newTodos = [...todos]
+
+    // 利用id值找到對應的todo項目的索引值
+    // 用findIndex的作法
+    const index = newTodos.findIndex((item) => item.id === id)
+
+    // 如果有找到就切換completed的true/false
     if (index !== -1) {
       newTodos[index].completed = !newTodos[index].completed
-
+      // 設定回狀態值
       setTodos(newTodos)
+    }
+
+    // // for迴圈的作法
+    // for (let i = 0; i < newTodos.length; i++) {
+    //   if (newTodos[i].id === id) {
+    //     newTodos[i].completed = !newTodos[i].completed
+    //     break
+    //   }
+    // }
+
+    // setTodos(newTodos)
+  }
+
+  const handleAddNew = (e) => {
+    if (e.key === 'Enter') {
+      // 建立一個todo項目的物件值
+      // 用時間物件轉微秒整數當id值
+      const newTodoItem = {
+        id: +new Date(),
+        text: e.target.value,
+        completed: false,
+      }
+
+      // 文字輸入框的值加到陣列todos
+      // 相當於unshift，在陣列前加入新的成員
+      const newTodos = [newTodoItem, ...todos]
+
+      // 設定陣列狀態值
+      setTodos(newTodos)
+
+      // 清空文字輸入框
+      setTodoInput('')
     }
   }
 
   return (
     <>
-      {/* 可控的表單元素 */}
-      <form action="">
-        <input
-          type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            const newTodoItem = {
-              id: +new Date(),
-              text: todo,
-              completed: false,
-            }
-
-            const newTodos = [newTodoItem, ...todos]
-            setTodos(newTodos)
-            setTodo('')
-          }}
-        >
-          Add
-        </button>
-      </form>
-      <ul>
-      {/* 用map的方式拿出todos裡的所有資料 */}
-        {todos.map((todoItem, i) => {
-          return (
-            <li key={todoItem.id}>
-              <input
-                type="checkbox"
-                checked={todos.complete}
-                onchang={() => {
-                  handelComplete(todoItem.id)
-                }}
-              ></input>
-              {/* 用completed判斷物件是否需要加完成線 */}
-              {todoItem.completed ? <del>{todoItem.text}</del> : todoItem.text}
-            </li>
-          )
-        })}
-      </ul>
+      {/* 可控的表單元素，value對應到狀態，onChange對應到設定狀態 */}
+      <TodoAddForm
+        todoInput={todoInput}
+        setTodoInput={setTodoInput}
+        handleAddNew={handleAddNew}
+      />
+      <TodoList
+        todos={todos}
+        handleCompleted={handleCompleted}
+        handleDelete={handleDelete}
+        handleEdited={handleEdited}
+      />
     </>
   )
 }
